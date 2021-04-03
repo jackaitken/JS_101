01c039fed21cf7567c0b6ce6aa7e7951314f7dce
Jack Aitken <jaitk11@gmail.com>
2021-04-02 21:22:52 -0400

    started working on CL calculator"

---
+++ lesson_2/calculator.js
@@ -0,0 +1,12 @@
+// Prompt for first number.
+// Prompt for second number.
+// Prompt for operation.
+// Perform the operation.
+// Print result in terminal.
+
+const readline = require('readline-sync');
+
+console.log('Welcome to Calculator!');
+
+console.log('What is the first number?')
+readline.question();
