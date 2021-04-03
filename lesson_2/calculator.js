973d9fda15405c2c9f762618aa95553645f2c87e
Jack Aitken <jaitk11@gmail.com>
2021-04-02 21:45:11 -0400

    finished calculator

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -9,4 +9,38 @@ const readline = require('readline-sync');
 console.log('Welcome to Calculator!');
 
 console.log('What is the first number?')
-readline.question();
+let number1 = readline.question();
+
+console.log('What is the second number?')
+let number2 = readline.question();
+
+console.log(
+  'What operation would you like to perform?\n1: Add\n2: Subtract\n3: Multiply\n4: Divide');
+
+let operation = readline.question();
+
+if (operation === '1') {
+  output = Number(number1) + Number(number2);
+} else if (operation === '2') { 
+  output = Number(number1) - Number(number2);
+} else if (operation === '3') { 
+  output = Number(number1) * Number(number2);
+} else if (operation === '4') {
+  output = Number(number1) / Number(number2);
+}
+
+// OR: 
+
+// let output;
+// switch (operation) {
+//   case '1': output = Number(number1) + Number(number2)
+//     break
+//   case '2': output = Number(number1) - Number(number2)
+//     break
+//   case '3': output = Number(number1) * Number(number2)
+//     break
+//   case '4': output = Number(number1) / Number(number2)
+//     break
+// }
+
+// console.log(`The result is: ${output}`);
