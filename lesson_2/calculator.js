37008bd3fe507f3a72c6a7e35aea840eb810aa9b
Jack Aitken <jaitk11@gmail.com>
2021-04-03 22:30:28 -0400

    Implemented loop to allow for more calculations

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -6,44 +6,69 @@ let invalidNumber = num => {
   return num.trimStart() === '' || Number.isNaN(Number(num));
 };
 
+let invalidLetter = char => {
+  return char === 'y' || char === 'n';
+};
+
 prompt('Welcome to Calculator!');
 
-prompt('What is the first number?');
-let number1 = readline.question();
+let calculateAgain = true;
 
-while (invalidNumber(number1)) {
-  prompt(`Please enter a valid number.`);
-  number1 = readline.question();
-}
+do {
+  prompt('What is the first number?');
+  let number1 = readline.question();
 
-prompt('What is the second number?');
-let number2 = readline.question();
+  while (invalidNumber(number1)) {
+    prompt(`Please enter a valid number.`);
+    number1 = readline.question();
+  }
 
-while (invalidNumber(number2)) {
-  prompt(`Please enter a valid number.`);
-  number2 = readline.question();
-}
+  prompt('What is the second number?');
+  let number2 = readline.question();
 
-prompt(
-  'What operation would you like to perform?\n1: Add\n2: Subtract\n3: Multiply\n4: Divide');
+  while (invalidNumber(number2)) {
+    prompt(`Please enter a valid number.`);
+    number2 = readline.question();
+  }
 
-let operation = readline.question();
+  prompt(
+    'What operation would you like to perform?\n1: Add\n2: Subtract\n3: Multiply\n4: Divide');
 
-while (invalidNumber(operation) || (operation < 1 || operation > 4)) {
-  prompt(`Please enter 1, 2, 3, or 4`);
-  operation = readline.question();
-}
+  let operation = readline.question();
 
-let output;
-switch (operation) {
-  case '1': output = Number(number1) + Number(number2);
-    break;
-  case '2': output = Number(number1) - Number(number2);
-    break;
-  case '3': output = Number(number1) * Number(number2);
-    break;
-  case '4': output = Number(number1) / Number(number2);
-    break;
-}
+  while (invalidNumber(operation) || (operation < 1 || operation > 4)) {
+    prompt(`Please enter 1, 2, 3, or 4`);
+    operation = readline.question();
+  }
 
-prompt(`The result is: ${output}`);
+  let output;
+  switch (operation) {
+    case '1': output = Number(number1) + Number(number2);
+      break;
+    case '2': output = Number(number1) - Number(number2);
+      break;
+    case '3': output = Number(number1) * Number(number2);
+      break;
+    case '4': output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(`The result is: ${output}`);
+
+  // Run another calculation or exit?
+  prompt(`Do you want to run another calculation? Type 'y' or 'n'.`);
+  let continueQuestion = readline.question();
+
+  while (!invalidLetter(continueQuestion)) {
+    prompt(`Please type 'y' or 'n'.`);
+    continueQuestion = readline.question();
+  }
+
+  if (continueQuestion.toLowerCase() === 'y') {
+    calculateAgain = true;
+  } else {
+    prompt(`Okay! See ya later.`);
+    calculateAgain = false;
+  }
+
+} while (calculateAgain);
