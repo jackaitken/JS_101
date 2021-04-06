954b46438475738e172d29a30965b13bc381ff2f
Jack Aitken <jaitk11@gmail.com>
2021-04-06 08:09:46 -0400

    Fixed validation bugs

--- lesson_2/loan_calculator.js
+++ lesson_2/loan_calculator.js
@@ -3,6 +3,14 @@ let MESSAGES = require('./loan_messages.json');
 
 let prompt = txt => console.log(`=> ${txt}`);
 
+let numberIsInvalid = amount => {
+  return Number.isNaN(Number(amount)) || amount < 1;
+};
+
+let nameIsValid = name => name.length !== 0;
+
+let continueValidation = key => key === 'y' || key === 'n';
+
 prompt(MESSAGES.welcome);
 
 while (true) {
@@ -10,19 +18,40 @@ while (true) {
   prompt(MESSAGES.loanName);
   let loanName = readline.question();
 
+  while (!nameIsValid(loanName)) {
+    prompt(MESSAGES.nameValidate);
+    loanName = readline.question();
+  }
+
   // Get loan amount
   prompt(`${MESSAGES.loanAmount} '${loanName}'?`);
   prompt(`${MESSAGES.amountValidate}`);
   let loanAmount = Number(readline.question('$ '));
 
+  while (numberIsInvalid(loanAmount)) {
+    prompt(`${MESSAGES.amountValidate}`);
+    loanAmount = Number(readline.question('$ '));
+  }
+
   // Get loan duration
-  prompt(MESSAGES.loanMonths);
+  prompt(MESSAGES.loanDuration);
   let loanMonths = Number(readline.question());
+
+  while (numberIsInvalid(loanMonths)) {
+    prompt(`${MESSAGES.loanDuration}`);
+    loanMonths = Number(readline.question('$ '));
+  }
 
   // Get APR and convert to workable number
   prompt(`${MESSAGES.APR} '${loanName}' loan?`);
   prompt(MESSAGES.APRValidate);
   let apr = Number(readline.question('% '));
+
+  while (numberIsInvalid(apr)) {
+    prompt(`${MESSAGES.APRValidate}`);
+    apr = Number(readline.question('$ '));
+  }
+
   apr /= 12;
   apr /= 100;
 
@@ -38,9 +67,23 @@ while (true) {
   let interestAmount = totalPayment - loanAmount;
   interestAmount = interestAmount.toFixed(2);
 
-  prompt(`Your monthly payment will be: $${monthlyPayment}`);
-  prompt(`You will pay a total of: $${interestAmount} in interest`);
-  prompt(`Your total payment will be: $${totalPayment}`);
+  prompt(`Your monthly payment will be: $${monthlyPayment}.`);
+  prompt(`You will pay a total of: $${interestAmount} in interest.`);
+  prompt(`Your total payment will be: $${totalPayment}.`);
 
-  break;
+  console.log('\n');
+
+  prompt(MESSAGES.another);
+  prompt(MESSAGES.yOrN);
+  let anotherLoan = readline.question();
+
+  while (!continueValidation(anotherLoan)) {
+    prompt(MESSAGES.yOrN);
+    anotherLoan = readline.question();
+  }
+
+  if (anotherLoan === 'n') {
+    prompt(MESSAGES.goodbye);
+    break;
+  }
 }
