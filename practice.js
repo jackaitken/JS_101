a0871371923f4539bba024e0704c4ebb9398e19a
Jack Aitken <jaitk11@gmail.com>
2021-04-22 21:39:31 -0400

    Working on array methods

--- practice.js
+++ practice.js
@@ -1,14 +1,7 @@
-const textObject = {
-  text: "Remove occurances of the letter e after the word 'letter' in this sentence"
+let animal = {
+  bear: 'grizzly',
+  fish: 'salmon',
+  dog: 'golden retriever'
 };
 
-Object.freeze(textObject);
-
-// My code below
-
-let textStr = Object.values(textObject).join('');
-const regex = /e/g;
-
-let resultStr = textStr.slice(0, 31) + textStr.slice(32).replace(regex, '');
-console.log(resultStr);
-
+console.log(Object.values(animal).some(value => value.length > 50));
