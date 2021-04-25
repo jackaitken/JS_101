034b4ada2f7f7ceeafcb73842e933ced3f1c739a
Jack Aitken <jaitk11@gmail.com>
2021-04-24 23:06:46 -0400

    still working on practice problems

--- practice.js
+++ practice.js
@@ -1,5 +1,18 @@
-let arr = [];
+let twoNumberSum = (arr, target) => {
+  let reciprocalObj = {};
 
-arr.length = 5;
-console.log(arr.length);
-console.log(Object.keys(arr));
+  arr.forEach(num => {
+    reciprocalObj[num] = target - num;
+  });
+
+  console.log(reciprocalObj);
+  console.log(Object.values(reciprocalObj));
+
+  Object.values(reciprocalObj).includes()
+};
+
+
+let numbers = [1, 2, 3, 9];
+let numbers1 = [1, 2, 4, 4];
+
+console.log(twoNumberSum(numbers1, 8));
