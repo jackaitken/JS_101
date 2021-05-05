5fddb1a16162bad91f0f8b345cc3c0d4f7605b3b
Jack Aitken <jaitk11@gmail.com>
2021-05-04 22:06:03 -0400

    Working through some practice problems

--- practice.js
+++ practice.js
@@ -1,57 +1,76 @@
-let st = "COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH";
-let we = [1, 4, 4, 5, 2, 1];
+let munsters = {
+  Herman: { age: 32, gender: 'male' },
+  Lily: { age: 30, gender: 'female' },
+  Grandpa: { age: 402, gender: 'male' },
+  Eddie: { age: 10, gender: 'male' },
+  Marilyn: { age: 23, gender: 'female'}
+};
 
-let nameArr = st.split(',');
-let nameObj = nameArr.reduce((arr, elem, index) => {
-  let length = elem.length;
-  let nameValue = elem.split('').reduce((acc, _, index) => {
-    let charValue = elem.toLowerCase().charCodeAt(index) - 96;
-    return acc + charValue;
-  }, 0);
-  arr.push([elem, (length + nameValue) * we[index]]);
-  return arr;
-}, []);
+Object.entries(munsters).forEach(munster => {
+  let name = munster[0];
+  let age = munster[1].age;
+  let gender = munster[1].gender;
 
-let sortedNames = nameObj.sort((a, b) => {
-  let sortValue = b[1] - a[1];
-  if (sortValue === 0) {
-    if (a[0] > b[0]) {
-      return 1;
-    } else {
-      return -1;
-    }
-  }
-  return sortValue;
+  console.log(`${name} is a ${age}-year-old ${gender}`);
 });
 
-console.log(sortedNames);
 
 
-// let b = 2;
-
-// function test(b) {
-//   return b += 5;
-// }
-
-// console.log(test());
 
 
 /*
-You're given an array of integers.  You must return the number of
-'arithmetic progressions' of size 3 that are possible from that list.
-
-In each progression, the differences between the elements must be the same.
-
-Example:
-[1, 2, 3, 5, 7, 9] ==> 5
-The above has 5 progressions, seen below:
-// [1, 2, 3], [1, 3, 5], [1, 5, 9], [3, 5, 7], and [5, 7, 9]
-
-All array elements will be unique and the array will be sorted.
+- We passed in both our northSouth coordinate and our eastWest coordinate.
+- we should check the value of the map at those coordinates.
+- Based on that value we can adjust northSouth or eastWest
 */
 
 
 
-// console.log(progressions([1, 2, 3, 5, 7, 9])); // 5
-// console.log(progressions([1, 2, 3, 4, 5]));    // 4
-// console.log(progressions([0, 5, 8, 9, 11, 13, 14, 16, 17, 19])); // 10
+
+// let arr1 = [[1, 2, 3], [4, 5, 6]]
+
+// arr1.map(elem => {
+//   return elem.map(num => { // [2, 3, 4]
+//     return num + 1;
+//   })
+// })
+
+
+// /*
+// On line 1, the global variable `arr1` is declared and initialized to an array literal `[[1, 2, 3], [4, 5, 6]]`.
+
+// On line 3, the `map` method is called on the array referenced by the global variable `arr1`. A callback function is passed as an argument to the `map` method. As `map` iterates over the items of the caller array, each item is assigned to the callback function's parameter `elem`. 
+
+// On line 4, the `map` method is called on the current value of the `elem` parameter, which references another array. A callback function is passed as an argument to the `map` method. As `map` iterates over the items of the caller array, each item is assigned to the callback function's parameter `num`. 
+
+// On line 5, the value stored in the `num` parameter is transformed by incrementing the value by `1`. Therefore, the value produced by the expression `num + 1` is explicitly returned.
+
+// The innermost map method will return the items [2, 3, 4], [5, 6, 7]
+
+// The `map` method transforms an array by 
+
+
+// The parameter `elem` references the current item in the caller array, which is a reference to an array. 
+// *
+
+
+
+// // let arr = [1, 2, 3, 4];
+
+// // arr.map(num => {
+// //   return num + 1;
+// // });
+
+
+// /*
+// On line 1, the global variable `arr` is declared and initialized to the array literal 
+// [1, 2, 3, 4]. 
+
+// On line 3, the `map` method is called on the array referenced by global variable `arr`. A callback function is passed as an argument to the map method. As the `map` method iterates over the items of the caller array, each item is assigned to the callback function's parameter `num`. 
+
+// On line 4, the value stored in the callback function's num parameter at the current iteration is transformed by incrementing the value by 1. Then this value is explicitly returned.
+
+// Explain `map`
+// Therefore, a new array [2, 3, 4, 5] is returned by the map method. 
+
+// */
