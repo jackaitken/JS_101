4d45481edea79d360c2f42cdd5f43fffe3a4570c
Jack Aitken <jaitk11@gmail.com>
2021-05-10 22:45:20 -0400

    Refactored some of the functions

--- lesson_6/tic_tac_toe.js
+++ lesson_6/tic_tac_toe.js
@@ -2,37 +2,10 @@ const readline = require('readline-sync');
 const EMPTY_SQUARE = ' ';
 const HUMAN_MARKER = 'X';
 const CPU_MARKER = '0';
-let currentPlayer = HUMAN_MARKER;
 let board = initializeBoard();
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
-}
-
-while (true) {
-  console.clear();
-  displayBoard(board);
-
-  prompt(`${currentPlayer}'s move`);
-  let row = getPlayerMove('row');
-  let column = getPlayerMove('column');
-
-  while (!isEmpty(row, column, board)) {
-    prompt('Please select an empty square');
-    row = getPlayerMove('row');
-    column = getPlayerMove('column');
-  }
-
-  board[row][column] = HUMAN_MARKER;
-
-  let [cpuRow, cpuCol] = getCpuMove(board);
-  board[cpuRow][cpuCol] = CPU_MARKER;
-  console.clear();
-
-  displayBoard(board);
-  prompt(`Computer chose row ${cpuRow + 1}, column ${cpuCol + 1}`);
-  prompt('Press enter to continue');
-  readline.prompt();
 }
 
 function initializeBoard() {
@@ -60,15 +33,17 @@ function displayBoard(boardState) {
   console.log('');
 }
 
-function getPlayerMove(colRow) {
-  prompt(`Select a ${colRow}`);
-  let move = readline.question('> ').trim();
+function getPlayerMove(rowCol) {
+  while (true) {
+    prompt(`Please choose a ${rowCol}`);
+    let move = readline.question('> ').trim();
 
-  while (!validMove(move)) {
-    prompt(`Select a ${colRow}`);
-    move = readline.question('> ').trim();
+    if (validMove(move)) {
+      move -= 1;
+      return move;
+    }
+    prompt('Please use a valid number (1 - 3)');
   }
-  return move - 1;
 }
 
 function validMove(move) {
@@ -96,4 +71,36 @@ function getCpuMove(board) {
   return validMoves[randIndx];
 }
 
+function isTie(board) {
+  return board.every(row => {
+    return row.every(cell => cell !== EMPTY_SQUARE);
+  });
+}
 
+while (true) {
+  console.clear();
+  displayBoard(board);
+
+  let row = getPlayerMove();
+  let column = getPlayerMove();
+
+  // Ensure that row and column are empty squares
+
+  board[row][column] = HUMAN_MARKER;
+
+  if (isTie(board)) {
+    console.clear();
+    displayBoard(board);
+    return prompt('Game ended in a tie');
+  }
+
+  let [cpuRow, cpuCol] = getCpuMove(board);
+  board[cpuRow][cpuCol] = CPU_MARKER;
+  console.clear();
+
+  displayBoard(board);
+  prompt(`Computer chose row ${cpuRow + 1}, column ${cpuCol + 1}`);
+
+  prompt('Press enter to continue');
+  readline.prompt();
+}
