f3f17c32bc5e12da551366b06a5ea44c7a137cb9
Jack Aitken <jaitk11@gmail.com>
2021-05-11 20:12:01 -0400

    test ttt functions

--- lesson_6/practice.js
+++ lesson_6/practice.js
@@ -1,24 +1,38 @@
-const EMPTY_SQUARE = ' ';
 
-function getCpuMove(board) {
-  let validMoves = board.map((row, rowIndx) => {
-    return row.reduce((arr, elem, colIndx) => {
-      if (elem === EMPTY_SQUARE) {
-        arr.push(rowIndx, colIndx);
-      }
-      return arr;
-    }, []);
-  });
-  return validMoves;
-
-  // let randIndx = Math.floor(Math.random() * validMoves.length);
-  // return validMoves[randIndx];
-}
+const HUMAN_MARKER = 'X';
+const CPU_MARKER = '0';
 
 let board = [
-  [' ', 'X', ' '],
-  [' ', 'O', 'X'],
-  [' ', ' ', ' ']
+  ['X', 'O', 'O'],
+  ['X', ' ', ' '],
+  ['X', ' ', 'O']
 ];
 
-console.log(getCpuMove(board));
+function detectWinner(board) {
+  let winningScenarios = [
+    [[0, 0], [0, 1], [0, 2]], [[1, 0], [1, 1], [1, 2]], // rows
+    [[2, 0], [2, 1], [2, 2]], 
+    [[0, 0], [1, 0], [2, 0]], [[1, 0], [1, 1], [1, 2]], // columns
+    [[2, 0], [2, 1], [2, 2]],
+    [[0, 0], [1, 1], [2, 2]], [[2, 0], [1, 1], [0, 2]], // diagonals
+  ];
+
+  for (let row of winningScenarios) {
+    let [sq1, sq2, sq3] = row;
+    if (
+      board[sq1[0]][sq1[1]] === HUMAN_MARKER &&
+      board[sq2[0]][sq2[1]] === HUMAN_MARKER &&
+      board[sq3[0]][sq3[1]] === HUMAN_MARKER
+    ) {
+      return 'Player';
+    } else if (
+      board[sq1[0]][sq1[1]] === CPU_MARKER &&
+      board[sq2[0]][sq2[1]] === CPU_MARKER &&
+      board[sq3[0]][sq3[1]] === CPU_MARKER
+    ) {
+      return 'Computer';
+    }
+  }
+}
+
+console.log(detectWinner(board));
