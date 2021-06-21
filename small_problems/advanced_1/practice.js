a1b6e2578d245b53ed2151e9526dd06a8f7b5153
Jack Aitken <jaitk11@gmail.com>
2021-06-21 14:47:32 -0400

    interview prep

--- small_problems/advanced_1/practice.js
+++ small_problems/advanced_1/practice.js
@@ -1,54 +1,17 @@
-do/*
-We need to transpose an matrix.
-
-For example:
-
-this input:
-  matrix = [
-    [1, 5, 8],
-    [4, 7, 2],
-    [3, 9, 6]
-  ];
-
-would return:
-[
-  [1, 4, 3],
-  [5, 7, 9],
-  [8, 2, 6]
-]
-
-So we construct each new array by taking the values that occupy the same index
-in every array
-
-so the elements at [0][0], [2][2] and [1][1] don't change
-
-The element at [0][1] becomes the element at [1][0]
-[0][2] becomes [2][0]
-[1][0] becomes [0][1]
-[2][0] becomes [0][2]
-
-and so on
-
-Algorithm:
-  - we need to create a copy of the array
-  - then we need to iterate through that copy using a nested loop
-    - at each index ij we should swap i and j
-
-*/
-
-function transpose(matrix) {
-  return matrix.map((row, rowIndx) => {
-    return row.map((_, colIndx) => {
-      return matrix[colIndx][rowIndx];
-    });
-  });
+const longestRepetition = s => {
+  let max = 0;
+  let char = ''; 
+  for (let i = 1, l = s.length, c = 1; i <= l; i++) {
+    if (s[i] === s[i - 1]) c++;
+    else {
+      if (c > max) {
+        max = c;
+        char = s[i - 1];
+      }
+      c = 1;
+    }
+  }
+  return [char, max];
 }
 
-let matrix = [
-  [1, 5, 8, 5],
-  [4, 7, 2, 0],
-  [3, 9, 6, 1]
-];
-
-console.log(transpose(matrix));
-console.log(matrix);
+console.log(longestRepetition('aaabccccccddd'));
