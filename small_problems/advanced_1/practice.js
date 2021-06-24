9490483006dfa1d5be29751485e4ec23bc33241a
Jack Aitken <jaitk11@gmail.com>
2021-06-24 12:41:47 -0400

    interview prep

--- small_problems/advanced_1/practice.js
+++ small_problems/advanced_1/practice.js
@@ -1,63 +1,58 @@
 /*
-Given two strings
-we need to determine if both strings
-have a matching substring
+Title case means:
+  1. each word is uppercased
+  2. unless it's an exception like 'of' is in all
+     lowercase
 
 Rules:
-  - only consider substrings that are longer than one letter
-  - looks like it should be case insensitive
-  - two empty strings do not share substrings
+  - the first word no matter what should be uppercased
+  - each subsequence word should have it's first letter
+    uppercased unless it exists in the minor words string
 
-Examples:
-  - 'Something' 'Home'
-  - 'om' and 'om' are the first matching substring
-
-supercalifragilisticexpialidocious
-SoundOfItIsAtrociou
-
-1234567
-541265
+Input:
+  - the string that needs to be converted
+  - a string of minor words that must be lowercased
+    again unless it's the first word
 
 Algorithm:
-  - first check that the input strings are longer than 1 character
-  - find ALL substrings of both str1 and str2 that are longer than
-    - 1 character
-  - make sure they're all lower case
-  - iterate over first set of substrings
-  - check if that substring is in the other set
+  - lowercase entire string
+  - lowercase minor words and split into an array
+  - split string by word
+  - iterate through the array
+    - if the index is 0:
+      - uppercase the first letter (helper?)
+    - check if it's in the minor words array
+    - if it is:
+      - continue
+    - if it isn't uppercase the first letter
+
+Helper algorithm:
+  - take as input the word
+  - return the word with an uppercased first character
 */
 
-function substringTest(str1, str2) {
-  let str1Substrs = getSubstrs(str1);
-  let str2Substrs = getSubstrs(str2);
-
-  for (let i = 0; i < str1Substrs.length; i++) {
-    if (str2Substrs.includes(str1Substrs[i])) {
-      return true;
-    }
+function titleCase(title, minorWords) {
+  if (!title) return '';
+  title = title.toLowerCase();
+  if (minorWords) {
+    minorWords = minorWords.toLowerCase().split(' ');
+  } else {
+    minorWords = '';
   }
-  return false;
+
+  return title.split(' ').map((word, index) => {
+    if (index === 0 || !minorWords.includes(word)) {
+      return upperCaseFirst(word);
+    }
+    return word;
+  }).join(' ');
 }
 
-function getSubstrs(string) {
-  let substrs = [];
-  string = string.toLowerCase();
-
-  for (let i = 0; i < string.length; i++) {
-    for (let j = i + 2; j < string.length; j++) {
-      substrs.push(string.slice(i, j));
-    }
-  }
-  return substrs;
+function upperCaseFirst(word) {
+  return word[0].toUpperCase() + word.slice(1);
 }
 
-
-console.log(substringTest('Something', 'Fun') === false);
-console.log(substringTest('Something', 'Home') === true);
-console.log(substringTest('Something', '') === false);
-console.log(substringTest('', 'Something') === false);
-console.log(substringTest('BANANA', 'banana') === true);
-console.log(substringTest('test', '111t') === false);
-console.log(substringTest('', '') === false);
-console.log(substringTest('1234567', '541265') === true);
-console.log(substringTest('supercalifragilisticexpialidocious', 'SoundOfItIsAtrociou') === true);
+console.log(titleCase('') === '');
+console.log(titleCase('a clash of KINGS', 'a an the of') === 'A Clash of Kings');
+console.log(titleCase('THE WIND IN THE WILLOWS', 'The In') === 'The Wind in the Willows');
+console.log(titleCase('the quick brown fox') === 'The Quick Brown Fox');
