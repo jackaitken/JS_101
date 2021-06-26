bb6be4c76bf89b63e7b8458702259c4989b20949
Jack Aitken <jaitk11@gmail.com>
2021-06-26 12:30:53 -0400

    interview prep

--- small_problems/advanced_1/practice.js
+++ small_problems/advanced_1/practice.js
@@ -1,24 +1,63 @@
 /*
-Given two array's a and b
-I need to remove all values from
-list a that exist in list b
+Only Duplicates (6 kyu)
+https://www.codewars.com/kata/5a1dc4baffe75f270200006b/train/javascript
 
-Rules:
-  - if the value exists more than once in a:
-    - then all occurances of that char should be removed
+Given a string, remove any characters that are unique from the string.
 
-Algorthim:
-  - iterate through list b:
-    - create new RegExp object for element
-    - replace each object with a #
-  - return a filtered list without the hashes
+Example:
+
+input: "abccdefee"
+
+output: "cceee"
+
+Given a string we need to remove any characters
+that only occur once.
+
+Algorithm:
+  - create an empty string var
+  - create a seen array
+  - iterate through the input string
+    - if char is in the seen arr
+      - add to the str var
+    - else
+      - add it to the seen arr
+  - return the string var
+
 */
 
-function arrayDiff(a, b) {
-  return a.filter(elem => !b.includes(elem));
+function onlyDuplicates(str) {
+  let resultStr = '';
+  let seenArr = [];
+
+  str.split('').forEach(char => {
+    if (seenArr.includes(char)) {
+      if (!resultStr.includes(char)) {
+        resultStr += char.repeat(2);
+      } else {
+        resultStr += char;
+      }
+    } else {
+      seenArr.push(char);
+    }
+  });
+  return resultStr;
 }
 
-console.log(arrayDiff([], [4,5])); //[], "a was [], b was [4,5]");
-console.log(arrayDiff([3,4], [3])); //[4], "a was [3,4], b was [3]");
-console.log(arrayDiff([1,8,2], [])); // [1,8,2], "a was [1,8,2], b was []");
-console.log(arrayDiff([1,2,3], [1,2])); // [3], "a was [1,2,3], b was [1,2]")
+function onlyDuplicates(str) {
+  let seenObj = {};
+  str.split('').forEach(char => {
+    seenObj[char] ? seenObj[char] += 1 : seenObj[char] = 1;
+  });
+
+  let nonUniques = Object.entries(seenObj).filter(pair => {
+    return pair[1] > 1;
+  });
+
+  return nonUniques.map(pair => pair[0].repeat(pair[1])).join('');
+}
+
+
+console.log(onlyDuplicates('abccdefee')); // 'cceee'
+console.log(onlyDuplicates('hello')); // 'll'
+console.log(onlyDuplicates('colloquial')); // 'ollol'
+console.log(onlyDuplicates('foundersandcoders')); // 'ondersndoders'
