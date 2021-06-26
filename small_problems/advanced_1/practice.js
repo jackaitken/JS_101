ff4b0fec3e2aeaeac9a38337605808bfc18f1503
Jack Aitken <jaitk11@gmail.com>
2021-06-25 20:21:14 -0400

    interview prep

--- small_problems/advanced_1/practice.js
+++ small_problems/advanced_1/practice.js
@@ -1,12 +1,24 @@
-function duplicateCount(text){
-  let letterObj = {};
-  
-  for (let i in text) {
-    if (letterObj[text[i].toLowerCase()]) {
-      letterObj[text[i].toLowerCase()] += 1;
-    } else {
-      letterObj[text[i]] = 1;
-    }
-  }
-  return Object.values(letterObj).filter(obj => obj > 1).length
+/*
+Given two array's a and b
+I need to remove all values from
+list a that exist in list b
+
+Rules:
+  - if the value exists more than once in a:
+    - then all occurances of that char should be removed
+
+Algorthim:
+  - iterate through list b:
+    - create new RegExp object for element
+    - replace each object with a #
+  - return a filtered list without the hashes
+*/
+
+function arrayDiff(a, b) {
+  return a.filter(elem => !b.includes(elem));
 }
+
+console.log(arrayDiff([], [4,5])); //[], "a was [], b was [4,5]");
+console.log(arrayDiff([3,4], [3])); //[4], "a was [3,4], b was [3]");
+console.log(arrayDiff([1,8,2], [])); // [1,8,2], "a was [1,8,2], b was []");
+console.log(arrayDiff([1,2,3], [1,2])); // [3], "a was [1,2,3], b was [1,2]")
