a4c636e609c356a947d65868164a18fd59b64877
Jack Aitken <jaitk11@gmail.com>
2021-06-26 13:00:22 -0400

    interview prep

--- small_problems/advanced_1/practice.js
+++ small_problems/advanced_1/practice.js
@@ -6,42 +6,17 @@ Given a string, remove any characters that are unique from the string.
 
 Example:
 
-input: "abccdefee"
+'colloquial' // 'ollol'
 
-output: "cceee"
+create an object which stores the frequency of chars
+iterate through the string again
+and remove that char if it only appears once
 
 Given a string we need to remove any characters
 that only occur once.
 
 Algorithm:
-  - create an empty string var
-  - create a seen array
-  - iterate through the input string
-    - if char is in the seen arr
-      - add to the str var
-    - else
-      - add it to the seen arr
-  - return the string var
-
 */
-
-function onlyDuplicates(str) {
-  let resultStr = '';
-  let seenArr = [];
-
-  str.split('').forEach(char => {
-    if (seenArr.includes(char)) {
-      if (!resultStr.includes(char)) {
-        resultStr += char.repeat(2);
-      } else {
-        resultStr += char;
-      }
-    } else {
-      seenArr.push(char);
-    }
-  });
-  return resultStr;
-}
 
 function onlyDuplicates(str) {
   let seenObj = {};
@@ -49,11 +24,9 @@ function onlyDuplicates(str) {
     seenObj[char] ? seenObj[char] += 1 : seenObj[char] = 1;
   });
 
-  let nonUniques = Object.entries(seenObj).filter(pair => {
-    return pair[1] > 1;
-  });
-
-  return nonUniques.map(pair => pair[0].repeat(pair[1])).join('');
+  return str.split('').filter(char => {
+    return seenObj[char] > 1;
+  }).join('');
 }
 
 
